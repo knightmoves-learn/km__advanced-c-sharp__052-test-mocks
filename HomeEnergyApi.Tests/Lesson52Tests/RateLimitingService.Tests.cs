0ce04ef2825deea58bd1482ea36f656c2d85a0ad
Isaac <isaac@knightmoves.org>
2025-10-31 14:15:53 -0500

    test

--- HomeEnergyApi.Tests/Lesson52Tests/RateLimitingService.Tests.cs
+++ HomeEnergyApi.Tests/Lesson52Tests/RateLimitingService.Tests.cs
@@ -1,53 +1,39 @@
 using HomeEnergyApi.Services;
 using HomeEnergyApi.Wrapper;
+using Moq;
 
 public class RateLimitingServiceTests
 {
     private DateTime currentDateTime;
     private RateLimitingService rateLimitingService;
-    private StubDateTimeWrapper stubDateTimeWrapper;
+    private Mock<IDateTimeWrapper> mockDateTimeWrapper;
 
     public RateLimitingServiceTests()
     {
         currentDateTime = DateTime.UtcNow;
-        stubDateTimeWrapper = new StubDateTimeWrapper(currentDateTime);
-        rateLimitingService = new(stubDateTimeWrapper);
+        mockDateTimeWrapper = new Mock<IDateTimeWrapper>();
+        rateLimitingService = new(mockDateTimeWrapper.Object);
     }
 
     [Fact]
     public void ShouldReturnTrueWhenItIsWeekend()
     {
         var initialTime = DateTime.Parse("2000-01-01 01:01:01");
-        stubDateTimeWrapper.SetUp(initialTime); // Saturday
+        mockDateTimeWrapper.Setup(d => d.UtcNow()).Returns(initialTime); // Saturday
         var result = rateLimitingService.IsWeekend();
         Assert.True(result);
+
+        mockDateTimeWrapper.Verify(d => d.UtcNow());
     }
 
     [Fact]
     public void ShouldReturnFalseWhenItIsWeekday()
     {
         var initialTime = DateTime.Parse("2000-01-03 01:01:01");
-        stubDateTimeWrapper.SetUp(initialTime); // Saturday
+        mockDateTimeWrapper.Setup(d => d.UtcNow()).Returns(initialTime); // Saturday
         var result = rateLimitingService.IsWeekend();
         Assert.False(result);
-    }
-}
 
-class StubDateTimeWrapper : IDateTimeWrapper
-{
-    private DateTime dateTime;
-
-    public StubDateTimeWrapper(DateTime dateTime)
-    {
-        this.dateTime = dateTime;
-    }
-
-    public void SetUp(DateTime dateTime)
-    {
-        this.dateTime = dateTime;
-    }
-    public DateTime UtcNow()
-    {
-        return dateTime;
+        mockDateTimeWrapper.Verify(d => d.UtcNow());
     }
 }
